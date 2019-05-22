cd03d9ae961d209646077b8e866b5844a567bb76
CHRISTOPHER LYELL <101355183@student.swin.edu.au>
2019-05-22 13:36:15 +1000

    Create tac.py

---
+++ Practice/tac.py
@@ -0,0 +1,9 @@
+game = [["0", "0", "0"],
+["0", "0", "0"],
+["0", "0", "0"]]
+print(game)
+# playerOne = input("Please select your position")
+# coOrd = playerOne.split(",")
+# co = int(coOrd[0])
+# coo = int(coOrd[1])
+game[1][1].insert("X")
