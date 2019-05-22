b2625f03251b4a7cdef0e99aa8b3b1cc6eae44f4
CHRISTOPHER LYELL <101355183@student.swin.edu.au>
2019-05-22 13:24:04 +1000

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+j = 0
+while j ==0 :
+    i = input("What happens when you throw a yellow rock in the red sea? ")
+    if i == "It gets wet":
+        print("That's right, well done")
+        j = 1
+    else:
+        print("That's wrong, please try ")
