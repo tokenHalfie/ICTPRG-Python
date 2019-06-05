7b41b14e5dc1b589792c7e0a47c16b15007a1873
CHRISTOPHER LYELL <101355183@student.swin.edu.au>
2019-06-05 14:15:22 +1000

    Create Fibo.py

---
+++ Fibo.py
@@ -0,0 +1,13 @@
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1)+fibo(n-2)
+
+a = open("Fibonacci.txt","w")
+for x in range(30):
+    a.write(str(fibo(x)) + "\n")
+    #a.write("\n")
+    
